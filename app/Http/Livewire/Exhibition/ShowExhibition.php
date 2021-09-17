57abda00fd70bfd81fd6fa24c00584646f4cb223
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-17 09:57:25 +0200

    Backend: exhibition

---
+++ app/Http/Livewire/Exhibition/ShowExhibition.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Exhibition;
+
+use Livewire\Component;
+
+class ShowExhibition extends Component
+{
+    public function render()
+    {
+        return view('livewire.exhibition.show-exhibition');
+    }
+}
