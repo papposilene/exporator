76e5446ae3a78040b33df120a2ce89d8563cb966
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-17 09:56:50 +0200

    Backend: museum

---
+++ app/Http/Livewire/Museum/ListMuseum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Museum;
+
+use Livewire\Component;
+
+class ListMuseum extends Component
+{
+    public function render()
+    {
+        return view('livewire.museum.list-museum');
+    }
+}
---
+++ app/Http/Livewire/Museum/ShowMuseum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Museum;
+
+use Livewire\Component;
+
+class ShowMuseum extends Component
+{
+    public function render()
+    {
+        return view('livewire.museum.show-museum');
+    }
+}
