e4d2592a42fe258efef64cb46c60c1ff9ffe45f2
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-17 10:49:03 +0200

    Backend: exhibition

---
+++ app/Http/Livewire/Modals/ImportExhibition.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Modals;
+
+use Livewire\Component;
+
+class ImportExhibition extends Component
+{
+    public function render()
+    {
+        return view('livewire.modals.import-exhibition');
+    }
+}
