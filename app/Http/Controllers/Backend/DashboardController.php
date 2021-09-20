38d85b17b2a0ba55fa57fa0d8a3b4fba9f1f46ac
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-20 18:02:49 +0200

    Backend: dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -14,6 +14,6 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('backend.dashboard');
+        return view('dashboard.index');
     }
 }
