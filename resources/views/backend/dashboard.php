0777b8d6ea86fc8bc4b3455cfbf21be016177678
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-20 17:35:16 +0200

    Backend: dashboard

--- resources/views/backend/dashboard.php
+++ resources/views/backend/dashboard.php
@@ -1,13 +1,13 @@
 <div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            @ucfirst(__('app.dashboard'))
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <backend.stat-country />
-        <backend.stat-museum />
-        <backend.stat-exhibition />
+        <livewire:backend.stat-country />
+        <livewire:backend.stat-museum />
+        <livewire:backend.stat-exhibition />
     </div>
 </div>
