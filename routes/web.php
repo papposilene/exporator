a124192df61f03b29a13de00096e51f88bb68ad2
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-20 19:11:21 +0200

    Backend: dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Livewire\Dashboard;
+use App\Http\Livewire\Dashboard\Dashboard;
 use App\Http\Controllers\Backend\ExhibitionController;
 use App\Http\Controllers\Backend\MuseumController;
 use App\Http\Livewire\Country\ListCountry;
