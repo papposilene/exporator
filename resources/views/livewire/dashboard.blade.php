b4c0dcd4c8603b370b807590b59670f9e35a7b0a
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-20 19:09:23 +0200

    Backend: dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,13 +1,3 @@
 <div>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            @ucfirst(__('app.dashboard'))
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <livewire:dashboard.stat-country />
-        <livewire:dashboard.stat-museum />
-        <livewire:dashboard.stat-exhibition />
-    </div>
+    <livewire:dashboard.stat-country />
 </div>
