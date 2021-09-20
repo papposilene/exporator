43bdf779e4b4d2a18a7e7d69d8620f1a5124a6cb
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-20 17:36:33 +0200

    Backend: dashboard

--- app/Http/Livewire/Backend/StatExhibition.php
+++ app/Http/Livewire/Backend/StatExhibition.php
@@ -5,10 +5,10 @@ namespace App\Http\Livewire\Backend;
 use App\Models\Exhibition;
 use Livewire\Component;
 
-class StatExhibitions extends Component
+class StatExhibition extends Component
 {
     public function render()
     {
-        return view('livewire.backend.stat-exhibitions');
+        return view('livewire.backend.stat-exhibition');
     }
 }
--- app/Http/Livewire/Backend/StatMuseum.php
+++ app/Http/Livewire/Backend/StatMuseum.php
@@ -5,10 +5,10 @@ namespace App\Http\Livewire\Backend;
 use App\Models\Museum;
 use Livewire\Component;
 
-class StatMuseums extends Component
+class StatMuseum extends Component
 {
     public function render()
     {
-        return view('livewire.backend.stat-museums');
+        return view('livewire.backend.stat-museum');
     }
 }
