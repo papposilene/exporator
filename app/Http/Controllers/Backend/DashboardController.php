a0dc49adea6eaab2881794596054c867897c9246
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-20 18:53:48 +0200

    Backend: dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -14,6 +14,6 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard.index');
+        return view('livewire.dashboard');
     }
 }
