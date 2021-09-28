71e1de1c14eeb7d8deb76ae6586d429c26d71af2
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-09-28 15:53:46 +0200

    Update show-dashboard.blade.php

--- resources/views/livewire/dashboard/show-dashboard.blade.php
+++ resources/views/livewire/dashboard/show-dashboard.blade.php
@@ -4,4 +4,10 @@
         <livewire:dashboard.stat-museum />
         <livewire:dashboard.stat-exhibition />
     </div>
+    <div class="flex flex-row justify-center w-full">
+        <livewire:dashboard.stat-map />
+    </div>
+    <div class="flex flex-row justify-center w-full">
+        <livewire:dashboard.stat-timeline />
+    </div>
 </div>
