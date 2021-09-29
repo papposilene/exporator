37603aa2e96e94c579922170c6af334f4c7d0013
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-09-29 17:09:11 +0200

    Create auth.php

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Les données d’authentification ne coorespondent à aucune de nos données.',
+    'password' => 'Le mot de passe ne semble pas être correct.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez patienter :seconds secondes avant de réessayer.',
+
+];
