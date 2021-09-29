fdf40e5a8d9ac519903be4daebf90d29c4afecb8
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-09-29 09:50:50 +0200

    Update app.js
    Add amCharts support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 require('./bootstrap');
 
+/* AlpineJS */
 import Alpine from 'alpinejs';
+
+/* amCharts */
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import * as am4plugins_timeline from '@amcharts/amcharts4/plugins/timeline';
 
 /* Leaflet */
 import 'leaflet/dist/leaflet-src.js'
