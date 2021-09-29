fbdee5cbd2331457c98069b6d2feb173078eefb7
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-29 19:17:37 +0200

    Frontend in progress

---
+++ app/Http/Livewire/BackendMenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class BackendMenu extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend-menu');
+    }
+}
