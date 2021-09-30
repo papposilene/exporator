f5a0c7524a91c5a450932c454db383f81db15e1b
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-09-30 21:39:31 +0200

    Routes for map and timeline

---
+++ app/Http/Livewire/Exhibition/MapExhibition.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Exhibition;
+
+use Livewire\Component;
+
+class MapExhibition extends Component
+{
+    public function render()
+    {
+        return view('livewire.exhibition.map-exhibition');
+    }
+}
---
+++ app/Http/Livewire/Exhibition/TimelineExhibition.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Exhibition;
+
+use Livewire\Component;
+
+class TimelineExhibition extends Component
+{
+    public function render()
+    {
+        return view('livewire.exhibition.timeline-exhibition');
+    }
+}
