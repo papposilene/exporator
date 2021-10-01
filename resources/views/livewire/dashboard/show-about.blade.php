0c5658b6f63ca9998bc7bed800d254733b81998d
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-10-01 16:48:07 +0200

    ESLint et Prettier

--- resources/views/livewire/dashboard/show-about.blade.php
+++ resources/views/livewire/dashboard/show-about.blade.php
@@ -16,22 +16,23 @@
                 trois formes différentes, à savoir :
                 <ol class="list-inside list-decimal mb-5">
                     <li>
-                        <a href="{{ route('front.exhibition.index') }}" class="text-blue-700 hover:text-red-600">une simple liste des expositions</a>,
+                        <a href="{{ route('front.exhibition.index') }}" class="text-blue-700 hover:text-red-600">une liste des expositions temporaires</a>,
                     </li>
                     <li>
                         <a href="{{ route('front.exhibition.map') }}" class="text-blue-700 hover:text-red-600">une cartographie des expositions en cours</a>,
                     </li>
                     <li>
-                        <a href="{{ route('front.exhibition.timeline') }}" class="text-blue-700 hover:text-red-600">enfin une chronologie des expositions fraîchement achevées, en cours et prochaines</a>.
+                        <a href="{{ route('front.exhibition.timeline') }}" class="text-blue-700 hover:text-red-600">une chronologie des expositions fraîchement achevées, en cours et prochaines</a>.
                     </li>
                 </ol>
             </p>
             <p class="mb-3">
-                Cette application web a été réalisée avec le <a href="https://laravel.com/docs/8.x"
+                Créée par <a href="https://dev.psln.nl/" class="text-blue-700 hover:text-red-600" target="_blank" rel="noopener">Philippe-Alexandre Pierre</a>
+                 en 2021, cette application web a été réalisée avec le <a href="https://laravel.com/docs/8.x"
                  class="text-blue-700 hover:text-red-600" target="_blank" rel="noopener">framework Laravel 8</a>,
                  <a href="https://laravel-livewire.com/docs/2.x/" class="text-blue-700 hover:text-red-600" target="_blank" rel="noopener">Livewire 2</a>
                  et <a href="https://tailwindcss.com/" class="text-blue-700 hover:text-red-600" target="_blank" rel="noopener">TailwindCSS 2</a>.
-                 Enfin, son code-source complet est <a href="https://github.com/papposilene/exporator"
+                 Enfin, son code-source complet, sous licence MIT, est <a href="https://github.com/papposilene/exporator"
                 class="text-blue-700 hover:text-red-600" target="_blank" rel="noopener">disponible sur Github</a>.
             </p>
         </div>
