8261c74b3eb8d1f01c3b657d3ac47fbd51067550
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-10-02 11:01:32 +0200

    Frontend: landing page

--- resources/views/livewire/dashboard/stat-tag.blade.php
+++ resources/views/livewire/dashboard/stat-tag.blade.php
@@ -1,16 +1,18 @@
 <div class="flex-grow bg-blue-100 p-5 shadow w-full">
     <h3 class="font-bold text-2xl mb-5">@ucfirst(__('app.tagcloud'))</h3>
+    @if(count($tags) > 0)
     <div class="grid sm:grid-cols-1 lg:grid-cols-5 gap-4">
-        @if(count($tags) > 0)
         @foreach($tags as $tag)
         <div class="bg-indigo-300 hover:bg-white m-2 p-2 rounded" title="@ucfirst(__('app.type_is', ['type' => $tag->type]))">
             <a href="{{ route('front.tag.show', ['slug' => $tag->slug]) }}" class="flex flex-grow justify-center">
                 {{ $tag->name }}
             </a>
         </div>
-        @endforeach
-        @else
-        @ucfirst(__('app.nothing')).
-        @endif
     </div>
+    @endforeach
+    @else
+    <div class="grid grid-cols-1 gap-4">
+        <p>@ucfirst(__('app.nothing')).</p>
+    </div>
+    @endif
 </div>
