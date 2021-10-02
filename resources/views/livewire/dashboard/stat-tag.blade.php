1cc6c72f11310a8ffa27a0df640dcb3f457b63a1
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-10-02 10:08:15 +0200

    Add support for tags

--- resources/views/livewire/dashboard/stat-tag.blade.php
+++ resources/views/livewire/dashboard/stat-tag.blade.php
@@ -1,6 +1,7 @@
 <div class="flex-grow bg-indigo-100 p-5 w-full">
     <h3 class="font-bold text-2xl mb-5">@ucfirst(__('app.tagcloud'))</h3>
     <div class="grid sm:grid-cols-1 lg:grid-cols-5 gap-4">
+        @if(count($tags) > 0)
         @foreach($tags as $tag)
         <div class="bg-indigo-300 hover:bg-white m-2 p-2 rounded" title="@ucfirst(__('app.type_is', ['type' => $tag->type]))">
             <a href="{{ route('front.tag.show', ['slug' => $tag->slug]) }}" class="flex flex-grow justify-center">
@@ -8,5 +9,8 @@
             </a>
         </div>
         @endforeach
+        @else
+        @ucfirst(__('app.nothing')).
+        @endif
     </div>
 </div>
