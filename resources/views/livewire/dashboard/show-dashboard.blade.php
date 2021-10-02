3e262966a1fc69dfd93ff774170ae39b4448577a
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-10-02 10:59:42 +0200

    Frontend: landing page

--- resources/views/livewire/dashboard/show-dashboard.blade.php
+++ resources/views/livewire/dashboard/show-dashboard.blade.php
@@ -7,13 +7,18 @@
 
     <div>
         <div class="flex flex-row flex-wrap max-w-7xl mx-auto py-5 px-6">
-            <div class="flex sm:flex-row lg:flex-col flex-grow bg-gray-400 lg:mr-3 p-5 shadow sm:w-full lg:w-1/4">
-                <p class="mb-3">
+            <div class="flex sm:flex-row lg:flex-col flex-grow bg-gray-300 lg:mr-3 p-5 shadow sm:w-full lg:w-1/4">
+                <p class="text-lg mb-3">
                     L'Exporateur, mot-valise entre <em>exposition</em> et <em>explorateur</em>, est un (énième ?)
                     site de référencement des expositions temporaires à Paris et ses proches alentours.
                 </p>
                 <p class="mb-3">
-
+                    Vous trouverez sur ce site les expositions temporaires actuellement en cours et futures (et
+                    celles déjà passées, mais bon...) disponibles sous la forme d’<a href="{{ route('front.exhibition.index') }}"
+                    class="text-blue-700 hover:text-red-600">une simple liste</a>, d’<a href="{{ route('front.exhibition.map') }}"
+                    class="text-blue-700 hover:text-red-600">une représentation cartographique</a> et
+                    d’<a href="{{ route('front.exhibition.timeline') }}" class="text-blue-700 hover:text-red-600">une
+                    représentation chronologique</a>.
                 </p>
             </div>
             <div class="flex sm:flex-row lg:flex-col flex-grow shadow sm:w-full lg:w-1/4">
