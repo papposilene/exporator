d957fb5a710cb637e949046973c1f9f19f9b3a8f
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-10-13 20:33:15 +0200

    Frontend: new calendar system

--- app/Http/Livewire/Exhibition/CalendarExhibition.php
+++ app/Http/Livewire/Exhibition/CalendarExhibition.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Livewire\Exhibition;
 
+use App\Models\Exhibition;
 use Livewire\Component;
 
 class CalendarExhibition extends Component
 {
+    public $page = 1;
+    public $search = '';
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'page' => ['except' => 1],
+    ];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.exhibition.calendar-exhibition');
+        setlocale(LC_TIME, app()->getLocale());
+        $timestamp = strtotime(date('Y-m-d'));
+        $year = date('Y');
+        $month = \Carbon\Carbon::month()->formatLocalized('%B');
+        $remaining_days = (int) date('t', $timestamp) - (int)date('j', $timestamp);
+        $today = date('Y-m-d');
+
+        $exhibitions = Exhibition::where('is_published', true)
+            ->where('began_at', '>', $today)
+            ->where('title', 'like', '%'.$this->search.'%')
+            ->orderBy('began_at', 'asc')
+            ->get();
+
+        return view('livewire.exhibition.calendar-exhibition', [
+            'year' => $year,
+            'month' => $month,
+            'remaining_days' => $remaining_days,
+            'exhibitions' => $exhibitions,
+        ]);
     }
 }
--- resources/views/livewire/exhibition/calendar-exhibition.blade.php
+++ resources/views/livewire/exhibition/calendar-exhibition.blade.php
@@ -6,9 +6,46 @@
     </x-slot>
 
     <div>
-        <div class="max-w-7xl mx-auto py-5 px-6">
-            <div id="calendar-container" class="w-full" wire:ignore>
-                <div id="calendar"></div>
+    <div class="max-w-7xl mx-auto py-5 px-6">
+            @if ($errors->any())
+            <div class="bg-red-400 border border-red-500 py-5 text-black">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+
+            <div class="relative flex items-center justify-between mb-2 w-full">
+                <div class="flex">
+
+                </div>
+                <x-forms.input wire:model="search" type="search" class="ml-2" :placeholder="@ucfirst(__('app.search'))" />
+            </div>
+            <div class="py-5 h-screen overflow-auto">
+                <h3 class="bg-gray-400 p-3 text-2xl font-bold">
+                    {{ $month }} {{ $year }}
+                </h3>
+                @for ($ii = 0; $ii < ($remaining_days + 1); $ii++)
+                @php
+                $day = \Carbon\Carbon::today()->add($ii, 'day');
+                $exhibitions_of_the_day = $exhibitions->where('began_at', '>=', $day->format('Y-m-d'));
+                @endphp
+                <h4 class="bg-gray-300 p-3 text-2xl font-bold">
+                    {{ $day->formatLocalized('%A %d %B') }}
+                </h4>
+                <ul>
+                    @foreach ($exhibitions_of_the_day as $exhibition_of_the_day)
+                    <li class="bg-gray-200 p-2">
+                        <a href="">
+                            <em>{{ $exhibition_of_the_day->inMuseum->name }}</em><br />
+                            {{ $exhibition_of_the_day->title }}
+                        </a>
+                    </li>
+                    @endforeach
+                </ul>
+                @endfor
             </div>
         </div>
     </div>
