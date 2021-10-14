84d403eabe467b6ec556d2465fd6b29bef687b27
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-10-14 03:11:22 +0200

    l10n

---
+++ resources/lang/en/date.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | DateTime Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the app for the days and
+    | months. You are free to modify these language lines according
+    | to your application's requirements.
+    |
+    */
+
+    'monday' => 'monday',
+    'tuesday' => 'tuesday',
+    'wednesday' => 'wednesday',
+    'thursday' => 'thursday',
+    'friday' => 'friday',
+    'saturday' => 'saturday',
+    'sunday' => 'sunday',
+
+    'january' => 'january',
+    'february' => 'february',
+    'march' => 'march',
+    'april' => 'april',
+    'mai' => 'mai',
+    'june' => 'june',
+    'july' => 'july',
+    'august' => 'august',
+    'september' => 'september',
+    'october' => 'october',
+    'november' => 'november',
+    'december' => 'december',
+];
