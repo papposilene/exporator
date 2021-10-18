dac91bfb28abfe5f6e257491fdb9ad4974248177
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-18 17:11:35 +0200

    Update stat-museum.blade.php

--- resources/views/livewire/dashboard/stat-museum.blade.php
+++ resources/views/livewire/dashboard/stat-museum.blade.php
@@ -2,13 +2,46 @@
     <h3 class="font-bold text-2xl mb-5">@ucfirst(__('app.museums'))</h3>
     <h4 class="font-bold text-1xl mb-2">@ucfirst(__('app.statistics'))</h4>
     <ol class="list-inside list-disc">
-        <li class="font-bold">@ucfirst(__('app.numbers_of_museums', ['count' => $museums]))</li>
-        <li>@ucfirst(__('app.numbers_of_museum_type', ['count' => $museum_type]))</li>
-        <li>@ucfirst(__('app.numbers_of_gallery_type', ['count' => $gallery_type]))</li>
-        <li>@ucfirst(__('app.numbers_of_artcenter_type', ['count' => $artcenter_type]))</li>
-        <li>@ucfirst(__('app.numbers_of_library_type', ['count' => $library_type]))</li>
-        <li>@ucfirst(__('app.numbers_of_foundation_type', ['count' => $foundation_type]))</li>
-        <li>@ucfirst(__('app.numbers_of_other_type', ['count' => $other_type]))</li>
+        <li class="font-bold">
+            <a href="{{ route('front.museum.index') }}" class="text-black hover:text-red-600">
+                @ucfirst(__('app.numbers_of_museums', ['count' => $museums]))
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('front.museum.index', ['filter' => 'museum']) }}" class="text-black hover:text-red-600">
+                @ucfirst(__('app.numbers_of_museum_type', ['count' => $museum_type]))
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('front.museum.index', ['filter' => 'gallery']) }}" class="text-black hover:text-red-600">
+                @ucfirst(__('app.numbers_of_gallery_type', ['count' => $gallery_type]))
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('front.museum.index', ['filter' => 'art-center']) }}" class="text-black hover:text-red-600">
+                @ucfirst(__('app.numbers_of_artcenter_type', ['count' => $artcenter_type]))
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('front.museum.index', ['filter' => 'art-fair']) }}" class="text-black hover:text-red-600">
+                @ucfirst(__('app.numbers_of_artfair_type', ['count' => $artfair_type]))
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('front.museum.index', ['filter' => 'library']) }}" class="text-black hover:text-red-600">
+                @ucfirst(__('app.numbers_of_library_type', ['count' => $library_type]))
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('front.museum.index', ['filter' => 'foundation']) }}" class="text-black hover:text-red-600">
+                @ucfirst(__('app.numbers_of_foundation_type', ['count' => $foundation_type]))
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('front.museum.index', ['filter' => 'other']) }}" class="text-black hover:text-red-600">
+                @ucfirst(__('app.numbers_of_other_type', ['count' => $other_type]))
+            </a>
+        </li>
     </ol>
     <!-- h4 class="font-bold text-1xl mt-5 mb-2">@ucfirst(__('app.no_exhibition'))</h4>
     <ol class="list-inside list-disc">
