eb6a5916489ed729b59460acdb4d036ec4826206
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-18 14:50:55 +0200

    Update show-exhibition.blade.php

--- resources/views/livewire/exhibition/show-exhibition.blade.php
+++ resources/views/livewire/exhibition/show-exhibition.blade.php
@@ -45,6 +45,9 @@
                     @endif
                 </li>
             </ul>
+            <ul class="bg-gray-200 list-inside md:m-5 p-5 w-full">
+                <li><livewire:interfaces.map :museum="$exhibition" :wire:key="$exhibition->uuid" /></li>
+            </ul>
             @auth
             @if (Auth::user()->can('create', App\Models\Exhibition::class))
             <ul class="bg-gray-200 list-inside md:m-5 p-5 w-full">
