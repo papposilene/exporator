ef5c4a396d9236fa0f8f610e86b85a2a293d5a02
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-18 15:28:42 +0200

    Update menu.blade.php

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -149,8 +149,14 @@
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <div class="ml-3 relative">
                     <div class="block px-4 py-2 text-xs text-gray-400">
+                        <a href="{{ route('register') }}" title="@ucfirst(__('auth.register'))"
+                            class="hover:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+                            </svg>
+                        </a>
                         <a href="{{ route('login') }}" title="@ucfirst(__('auth.login'))"
-                            class="hover:text-red-500">
+                            class="hover:text-black">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
                             </svg>
