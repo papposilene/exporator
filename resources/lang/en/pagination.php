ee3951991422f94ee4709d84e448762e751dc39e
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-18 16:25:50 +0200

    Update pagination.php

--- resources/lang/en/pagination.php
+++ resources/lang/en/pagination.php
@@ -15,5 +15,10 @@ return [
 
     'previous' => '&laquo; Previous',
     'next' => 'Next &raquo;',
+    
+    'showing' => 'Showing',
+    'to' => 'to',
+    'from' => 'from',
+    'results' => 'results',
 
 ];
