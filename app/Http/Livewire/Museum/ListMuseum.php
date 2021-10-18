5ff8c1fa4deccb0001e149f4d13ed5017ea899f4
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-18 15:42:32 +0200

    Update ListMuseum.php
    Add type filter for museum.

--- app/Http/Livewire/Museum/ListMuseum.php
+++ app/Http/Livewire/Museum/ListMuseum.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Museum;
 
 use App\Models\Museum;
+use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,12 +11,14 @@ class ListMuseum extends Component
 {
     use WithPagination;
 
+    public $filter = '';
     public $page = 1;
     public $search = '';
 
     protected $queryString = [
-        'search' => ['except' => ''],
+        'filter' => ['except' => ''],
         'page' => ['except' => 1],
+        'search' => ['except' => ''],
     ];
 
     public function updatingSearch()
@@ -25,11 +28,23 @@ class ListMuseum extends Component
 
     public function render()
     {
-        $museums = Museum::withCount('hasExhibitions')
-            ->where('name', 'like', '%'.$this->search.'%')
-            ->orderBy('has_exhibitions_count', 'desc')
-            ->orderBy('name', 'asc')
-            ->paginate(25);
+        if (Str::of($this->filter)->trim()->isNotEmpty())
+        {
+            $museums = Museum::withCount('hasExhibitions')
+                ->where('type', $this->filter)
+                ->where('name', 'like', '%'.$this->search.'%')
+                ->orderBy('has_exhibitions_count', 'desc')
+                ->orderBy('name', 'asc')
+                ->paginate(25);
+        }
+        else
+        {
+            $museums = Museum::withCount('hasExhibitions')
+                ->where('name', 'like', '%'.$this->search.'%')
+                ->orderBy('has_exhibitions_count', 'desc')
+                ->orderBy('name', 'asc')
+                ->paginate(25);
+        }
 
         return view('livewire.museum.list-museum', [
             'museums' => $museums,
