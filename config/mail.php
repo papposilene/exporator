fa46f1409754e24f0c592c4195d63f4075826325
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-10-19 10:52:35 +0200

    Config: mail server

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@exporateur.fr'),
+        'name' => env('MAIL_FROM_NAME', 'L’Exporateur'),
     ],
 
     /*
