a5ab230c450b8918678d04e1e4c0f7ccead3822d
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-19 16:45:45 +0200

    Update import-exhibition.blade.php

--- resources/views/livewire/modals/import-exhibition.blade.php
+++ resources/views/livewire/modals/import-exhibition.blade.php
@@ -68,6 +68,11 @@
                             </tr>
                             <tr class="border-b border-black border-dashed"
                                 title="@ucfirst(__('app.optional'))">
+                                <td class="bg-green-200 font-bold text-center">price</td>
+                                <td class="p-2">@ucfirst(__('app.price'))</td>
+                            </tr>
+                            <tr class="border-b border-black border-dashed"
+                                title="@ucfirst(__('app.optional'))">
                                 <td class="bg-green-200 font-bold text-center">tags</td>
                                 <td class="p-2">type1:tag1,type1:tag2,type2,tag:1</td>
                             </tr>
