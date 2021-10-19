db9607ab6ac9590f2bd1acc0a3f63b4784a316e6
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-19 16:42:49 +0200

    Create UpdateExhibitionRequest.php

---
+++ app/Http/Requests/UpdateExhibitionRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateExhibitionRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'uuid' => 'required|bail|uuid',
+            'museum' => 'required|bail|uuid',
+            'title' => 'required|string|max:255',
+            'began_at' => 'required|date_format:d/m/Y',
+            'ended_at' => 'required|date_format:d/m/Y',
+            'description' => 'required|string',
+            'link' => 'nullable|url',
+            'price' => 'nullable|integer',
+            'is_published' => 'required|boolean',
+        ];
+    }
+}
