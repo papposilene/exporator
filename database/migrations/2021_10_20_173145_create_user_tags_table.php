d71d421007f7f2adf1713c0c03ec6fee63a9334e
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-20 17:31:05 +0200

    Create 2021_10_20_173145_create_user_tags_table.php

---
+++ database/migrations/2021_10_20_173145_create_user_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_tags', function (Blueprint $table) {
+            $table->uuid('uuid')->primary();
+            $table->uuid('user_uuid');
+            $table->foreign('user_uuid')->references('uuid')->on('users');
+            $table->uuid('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_tags');
+    }
+}
