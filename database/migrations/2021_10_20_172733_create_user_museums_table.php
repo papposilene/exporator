7427f056d83d9c5c81bd650c93a074bddfb844b4
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-20 17:29:14 +0200

    Update 2021_10_20_172733_create_user_museums_table.php

--- database/migrations/2021_10_20_172733_create_user_museums_table.php
+++ database/migrations/2021_10_20_172733_create_user_museums_table.php
@@ -1,1 +1,36 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserMuseumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_museums', function (Blueprint $table) {
+            $table->uuid('uuid')->primary();
+            $table->uuid('user_uuid');
+            $table->foreign('user_uuid')->references('uuid')->on('users');
+            $table->uuid('museum_uuid');
+            $table->foreign('museum_uuid')->references('uuid')->on('museums');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_museums');
+    }
+}
