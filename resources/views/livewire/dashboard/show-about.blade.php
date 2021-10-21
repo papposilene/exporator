046e7c5ff2326b271b5bfb0bf53ca941d8949aa1
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-21 14:44:18 +0200

    Update show-about.blade.php

--- resources/views/livewire/dashboard/show-about.blade.php
+++ resources/views/livewire/dashboard/show-about.blade.php
@@ -24,7 +24,7 @@
                         <a href="{{ route('front.exhibition.map') }}" class="text-blue-700 hover:text-red-600">une cartographie des expositions en cours</a>,
                     </li>
                     <li>
-                        <a href="{{ route('front.exhibition.calendar') }}" class="text-blue-700 hover:text-red-600">un agenda des expositions fraîchement achevées, en cours et prochaines</a>.
+                        <a href="{{ route('front.exhibition.calendar') }}" class="text-blue-700 hover:text-red-600">un agenda des expositions pour le mois en cours</a>.
                     </li>
                 </ol>
             </p>
