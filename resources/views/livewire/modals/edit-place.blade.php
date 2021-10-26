0695a114da2f84febdb799e33d398d96c10e4d3f
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-26 16:12:43 +0200

    Update and rename edit-museum.blade.php to edit-place.blade.php

--- resources/views/livewire/modals/edit-place.blade.php
+++ resources/views/livewire/modals/edit-place.blade.php
@@ -1,17 +1,17 @@
-<div id="modalEditMuseum">
-    <div id="modalButtonEditMuseum">
-        <button id="modalOpenEditMuseum" class="focus:outline-none bg-purple-100 text-black bg-opacity-75 p-2 rounded w-full"
-            type="button" title="@ucfirst(__('app.edit_the', ['what' => __('app.museums')]))">
-            @ucfirst(__('app.edit_the', ['what' => __('app.museum')]))
+<div id="modalEditPlace">
+    <div id="modalButtonEditPlace">
+        <button id="modalOpenEditPlace" class="focus:outline-none bg-purple-100 text-black bg-opacity-75 p-2 rounded w-full"
+            type="button" title="@ucfirst(__('app.edit_the', ['what' => __('app.places')]))">
+            @ucfirst(__('app.edit_the', ['what' => __('app.place')]))
         </button>
     </div>
 
-    <div id="modalWindowEditMuseum"
+    <div id="modalWindowEditPlace"
         class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-blue-500 bg-opacity-50 transform scale-0 transition-transform duration-300 z-10">
         <!-- Modal -->
         <div class="bg-white overflow-auto w-1/2 h-1/2 p-12">
             <!-- Close modal button-->
-            <button id="modalCloseEditMuseum" type="button" class="focus:outline-none float-right">
+            <button id="modalCloseEditPlace" type="button" class="focus:outline-none float-right">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -19,27 +19,27 @@
                 </svg>
             </button>
             <!-- Modal content -->
-            <form method="POST" action="{{ route('admin.museum.update') }}" enctype="multipart/form-data"
+            <form method="POST" action="{{ route('admin.place.update') }}" enctype="multipart/form-data"
                 class="flex flex-col w-full">
                 @csrf
 
-                <x-forms.input type="hidden" name="uuid" value="{{ $museum->uuid }}" />
+                <x-forms.input type="hidden" name="uuid" value="{{ $place->uuid }}" />
 
                 <div class="mt-4">
-                    <x-forms.label for="name">@ucfirst(__('app.museum'))</x-forms.label>
-                    <x-forms.input id="name" class="block mt-1 w-full" type="text" name="name" required value="{{ $museum->name }}" />
+                    <x-forms.label for="name">@ucfirst(__('app.place'))</x-forms.label>
+                    <x-forms.input id="name" class="block mt-1 w-full" type="text" name="name" required value="{{ $place->name }}" />
                 </div>
 
                 <div class="mt-4">
                     <x-forms.label for="slug">@ucfirst(__('app.slug'))</x-forms.label>
-                    <x-forms.input id="slug" class="block mt-1 w-full" type="text" name="slug" required value="{{ $museum->slug }}" />
+                    <x-forms.input id="slug" class="block mt-1 w-full" type="text" name="slug" required value="{{ $place->slug }}" />
                 </div>
 
                 <div class="mt-4">
                     <x-forms.label for="type">@ucfirst(__('app.type'))</x-forms.label>
                     <x-forms.select id="type" class="block mt-1 w-full" name="type" required>
                         @foreach ($types as $type)
-                        <option value="{{ $type->slug }}" {{ ($type->slug === $museum->type ? 'selected=true' : '') }}>@ucfirst($type->type)</option>
+                        <option value="{{ $type->slug }}" {{ ($type->slug === $place->type ? 'selected=true' : '') }}>@ucfirst($type->type)</option>
                         @endforeach
                     </x-forms.select>
                 </div>
@@ -47,33 +47,33 @@
                 <div class="mt-4">
                     <x-forms.label for="status">@ucfirst(__('app.status'))</x-forms.label>
                     <x-forms.select id="status" class="block mt-1 w-full" name="status" required>
-                        <option value="true" {{ ($museum->status === 1 ? 'selected=true' : '') }}>@ucfirst(__('app.museum_open'))</option>
-                        <option value="false" {{ ($museum->status === 0 ? 'selected=true' : '') }}>@ucfirst(__('app.museum_close'))</option>
+                        <option value="true" {{ ($place->status === 1 ? 'selected=true' : '') }}>@ucfirst(__('app.place_open'))</option>
+                        <option value="false" {{ ($place->status === 0 ? 'selected=true' : '') }}>@ucfirst(__('app.place_close'))</option>
                     </x-forms.select>
                 </div>
 
                 <div class="mt-4">
                     <x-forms.label for="address">@ucfirst(__('app.address'))</x-forms.label>
-                    <x-forms.textarea id="address" class="block mt-1 w-full" type="text" name="address" required>{{ $museum->address }}</x-forms.textarea>
+                    <x-forms.textarea id="address" class="block mt-1 w-full" type="text" name="address" required>{{ $place->address }}</x-forms.textarea>
                 </div>
 
                 <div class="grid grid-cols-2 gap-x-4 mt-4">
                     <x-forms.label for="name">@ucfirst(__('app.city'))</x-forms.label>
                     <x-forms.label for="name">@ucfirst(__('app.country'))</x-forms.label>
-                    <x-forms.input id="city" class="block mt-1 w-full" type="text" name="city" required value="{{ $museum->city }}" />
+                    <x-forms.input id="city" class="block mt-1 w-full" type="text" name="city" required value="{{ $place->city }}" />
                     <livewire:country.autocomplete-country />
                 </div>
 
                 <div class="grid grid-cols-2 gap-x-4 mt-4">
                     <x-forms.label for="latitude">@ucfirst(__('app.latitude'))</x-forms.label>
                     <x-forms.label for="longitude">@ucfirst(__('app.longitude'))</x-forms.label>
-                    <x-forms.input id="latitude" class="block mt-1 w-full" type="text" name="latitude" required value="{{ $museum->lat }}" />
-                    <x-forms.input id="longitude" class="block mt-1 w-full" type="text" name="longitude" required value="{{ $museum->lon }}" />
+                    <x-forms.input id="latitude" class="block mt-1 w-full" type="text" name="latitude" required value="{{ $place->lat }}" />
+                    <x-forms.input id="longitude" class="block mt-1 w-full" type="text" name="longitude" required value="{{ $place->lon }}" />
                 </div>
 
                 <div class="mt-4">
                     <x-forms.label for="link">@ucfirst(__('app.link'))</x-forms.label>
-                    <x-forms.input id="link" class="block mt-1 w-full" type="text" name="link" required value="{{ $museum->link }}" />
+                    <x-forms.input id="link" class="block mt-1 w-full" type="text" name="link" required value="{{ $place->link }}" />
                 </div>
                 
                 <div class="mt-4">
@@ -93,11 +93,11 @@
 
 <script>
 document.addEventListener('livewire:load', function () {
-    const modalOpenEditMuseum = document.getElementById('modalOpenEditMuseum')
-    const modalCloseEditMuseum = document.getElementById('modalCloseEditMuseum')
-    const modalWindowEditMuseum = document.getElementById('modalWindowEditMuseum')
+    const modalOpenEditPlace = document.getElementById('modalOpenEditPlace')
+    const modalCloseEditPlace = document.getElementById('modalCloseEditPlace')
+    const modalWindowEditPlace = document.getElementById('modalWindowEditPlace')
 
-    modalOpenEditMuseum.addEventListener('click',()=>modalWindowEditMuseum.classList.remove('scale-0'))
-    modalCloseEditMuseum.addEventListener('click',()=>modalWindowEditMuseum.classList.add('scale-0'))
+    modalOpenEditPlace.addEventListener('click',()=>modalWindowEditPlace.classList.remove('scale-0'))
+    modalCloseEditPlace.addEventListener('click',()=>modalWindowEditPlace.classList.add('scale-0'))
 })
 </script>
