657a47e43b456165c5a16816139de1dbbd1754c7
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-26 15:50:28 +0200

    Update and rename UnfollowMuseumRequest.php to UnfollowPlaceRequest.php

--- app/Http/Requests/UnfollowPlaceRequest.php
+++ app/Http/Requests/UnfollowPlaceRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UnfollowMuseumRequest extends FormRequest
+class UnfollowPlaceRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
