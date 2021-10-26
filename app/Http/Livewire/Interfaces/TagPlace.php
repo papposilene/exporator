dfd6a840f2ff6c1e7c9add4216ecccbb3366d78b
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-26 15:59:34 +0200

    Update and rename TagMuseum.php to TagPlace.php

--- app/Http/Livewire/Interfaces/TagPlace.php
+++ app/Http/Livewire/Interfaces/TagPlace.php
@@ -4,10 +4,10 @@ namespace App\Http\Livewire\Interfaces;
 
 use Livewire\Component;
 
-class TagMuseum extends Component
+class TagPlace extends Component
 {
     public function render()
     {
-        return view('livewire.interfaces.tag-museum');
+        return view('livewire.interfaces.tag-place');
     }
 }
