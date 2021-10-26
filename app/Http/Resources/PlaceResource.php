212d92ea40f74d6eab42f8437b156dd0875a0f76
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-26 15:41:02 +0200

    Update and rename MuseumResource.php to PlaceResource.php

--- app/Http/Resources/PlaceResource.php
+++ app/Http/Resources/PlaceResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class MuseumResource extends JsonResource
+class PlaceResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
