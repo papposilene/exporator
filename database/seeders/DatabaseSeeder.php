b1fede8813a68d679a792686ae0bd998def0771e
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-27 14:52:34 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            TeamSeeder::class,
             CountrySeeder::class,
             TypeSeeder::class,
             TagSeeder::class,
