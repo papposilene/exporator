c01e56d2dfc01d9869e94a8bda7d1111da8dc69f
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-27 15:09:51 +0200

    Update JetstreamServiceProvider.php

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -64,7 +64,7 @@ class JetstreamServiceProvider extends ServiceProvider
             'update',
         ])->description('Editor users have the ability to read, create, and update.');
         
-        Jetstream::role('basic', 'User', [
+        Jetstream::role('user', 'User', [
             'read',
         ])->description('Basic users have the ability to read.');
     }
