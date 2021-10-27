2e82f221e0aa47f11f5bfb841d2ebdfd8fbcf597
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-27 11:42:57 +0200

    Update StatUser.php

--- app/Http/Livewire/Dashboard/StatUser.php
+++ app/Http/Livewire/Dashboard/StatUser.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\Dashboard;
 
+use App\Models\User;
 use Livewire\Component;
 
 class StatUser extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard.stat-user');
+        $user = User::findOrFail(Auth::id());
+        
+        return view('livewire.dashboard.stat-user', 
+            compact(
+                'user',
+            );
     }
 }
