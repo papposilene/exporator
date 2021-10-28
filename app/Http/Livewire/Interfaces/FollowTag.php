fd4de00546e93e4f11deb2383ec96e5063c40d3e
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-28 15:53:07 +0200

    Update FollowTag.php

--- app/Http/Livewire/Interfaces/FollowTag.php
+++ app/Http/Livewire/Interfaces/FollowTag.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire\Interfaces;
 
+use App\Models\Tag;
 use Livewire\Component;
 
 class FollowTag extends Component
 {
+    public Tag $tag;
+
+    public function mount($tag)
+    {
+        $this->tag = $tag;
+    }
+
     public function render()
     {
-        return view('livewire.interfaces.follow-tag');
+        return view('livewire.interfaces.follow-tag', [
+            'tag' => $this->tag,
+        ]);
     }
 }
