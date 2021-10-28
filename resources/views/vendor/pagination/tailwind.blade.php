dae93c5d8fd24bf3ff11c96dcf4b73b825aa6fdc
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-28 12:11:34 +0200

    Update tailwind.blade.php

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -25,13 +25,13 @@
         <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
             <div>
                 <p class="text-sm text-gray-700 leading-5">
-                    {!! __('Showing') !!}
+                    {{ __('pagination.showing') }}
                     <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                    {!! __('to') !!}
+                    {{ __('pagination.to') }}
                     <span class="font-medium">{{ $paginator->lastItem() }}</span>
-                    {!! __('of') !!}
+                    {{ __('pagination.of') }}
                     <span class="font-medium">{{ $paginator->total() }}</span>
-                    {!! __('results') !!}
+                    {{ __('pagination.results') }}
                 </p>
             </div>
 
