c662d10ff5b0b0d918f136733fd2eab735a9f985
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-10-28 15:53:25 +0200

    Update FollowExhibition.php

--- app/Http/Livewire/Interfaces/FollowExhibition.php
+++ app/Http/Livewire/Interfaces/FollowExhibition.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire\Interfaces;
 
+use App\Models\Exhibition;
 use Livewire\Component;
 
 class FollowExhibition extends Component
 {
+    public Exhibition $exhibition;
+
+    public function mount($exhibition)
+    {
+        $this->exhibition = $exhibition;
+    }
+
     public function render()
     {
-        return view('livewire.interfaces.follow-exhibition');
+        return view('livewire.interfaces.follow-exhibition', [
+            'exhibition' => $this->exhibition,
+        ]);
     }
 }
