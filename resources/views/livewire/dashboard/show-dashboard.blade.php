f5e5e0e0083471e3479a6f84adb7b9c3e03a4047
Philippe-Alexandre Pierre <dev@psln.nl>
2021-11-01 11:00:13 +0100

    frontend(dashboard): shadow

--- resources/views/livewire/dashboard/show-dashboard.blade.php
+++ resources/views/livewire/dashboard/show-dashboard.blade.php
@@ -38,10 +38,10 @@
             @endauth
         </div>
         <div class="flex flex-col lg:flex-row flex-wrap max-w-7xl mx-auto -mt-7 py-5 px-6">
-            <div class="flex flex-grow lg:flex-grow-0 lg:pr-2 flex-wrap shadow w-full lg:w-2/4">
+            <div class="flex flex-grow lg:flex-grow-0 lg:pr-2 flex-wrap w-full lg:w-2/4">
                 <livewire:dashboard.stat-place />
             </div>
-            <div class="flex flex-grow lg:flex-grow-0 flex-wrap shadow w-full lg:w-2/4">
+            <div class="flex flex-grow lg:flex-grow-0 flex-wrap w-full lg:w-2/4">
                 <livewire:dashboard.stat-exhibition />
             </div>
         </div>
--- resources/views/livewire/dashboard/stat-place.blade.php
+++ resources/views/livewire/dashboard/stat-place.blade.php
@@ -1,4 +1,4 @@
-<div class="flex-grow bg-purple-100 p-5 w-full">
+<div class="flex-grow bg-purple-100 p-5 shadow w-full">
     <h3 class="font-bold text-2xl mb-5">@ucfirst(__('app.places'))</h3>
     <h4 class="font-bold text-1xl mb-2">@ucfirst(__('app.statistics'))</h4>
     <ol class="list-inside list-disc">
