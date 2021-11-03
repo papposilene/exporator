ce550a4da5c9bf4186c1596295fa4f01ecbde863
Philippe-Alexandre Pierre <dev@psln.nl>
2021-11-03 21:22:38 +0100

    backend(place): delete

---
+++ app/Http/Livewire/Interfaces/DeletePlace.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Interfaces;
+
+use Livewire\Component;
+
+class DeletePlace extends Component
+{
+    public function render()
+    {
+        return view('livewire.interfaces.delete-place');
+    }
+}
