875dc7011a627bc395d9cefad8d2b4657c912079
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-04 09:31:14 +0100

    Update publish-exhibition.blade.php

--- resources/views/livewire/interfaces/publish-exhibition.blade.php
+++ resources/views/livewire/interfaces/publish-exhibition.blade.php
@@ -2,7 +2,7 @@
     <form method="POST" action="{{ route('admin.exhibition.publish') }}" class="flex justify-center w-full">
         @csrf
 
-        <input type="hidden" name="uuid" value="{{ $exhibition->uuid }}" />
+        <input type="hidden" name="exhibition" value="{{ $exhibition->uuid }}" />
 
         <x-forms.button class="bg-red-600 hover:bg-black">
             @ucfirst(__('app.publish_', ['what' => $exhibition->title]))
