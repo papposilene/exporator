06387ed20090d45630159e0cccf040071c415e12
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-04 13:52:21 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
-    x-cloak
+    prefix="og: http://ogp.me/ns#" x-cloak
     x-data="{darkMode: localStorage.getItem('dark') === 'true'}"
     x-init="$watch('darkMode', val => localStorage.setItem('dark', val))"
     x-bind:class="{'dark': darkMode}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <meta name="robots" content="index, follow" />
         <title>{{ config('app.name', 'Exporator') }}</title>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
