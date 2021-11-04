e0c51cac4b76c094fb547fb1792c859317049719
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-04 15:33:09 +0100

    Create chart.php

---
+++ resources/lang/fr/chart.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Chart Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the app for the charts.
+    | You are free to modify these language lines according
+    | to your application's requirements.
+    |
+    */
+
+    'places_by_types' => 'nombre de lieux par typologies',
+
+];
