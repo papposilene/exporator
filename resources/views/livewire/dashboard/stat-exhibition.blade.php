b990d7e4af8c4877f068d3fcd179cb21bc9b85c7
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-05 15:50:43 +0100

    Update stat-exhibition.blade.php

--- resources/views/livewire/dashboard/stat-exhibition.blade.php
+++ resources/views/livewire/dashboard/stat-exhibition.blade.php
@@ -1,5 +1,9 @@
 <div class="flex-grow bg-sky-300 p-5 shadow w-full">
-    <h3 class="font-bold text-2xl mb-5">@ucfirst(__('app.exhibitions'))</h3>
+    <h3 class="font-bold text-2xl mb-5">
+        <a href="{{ route('front.tag.index') }}">
+            @ucfirst(__('app.exhibitions'))
+        </a>
+    </h3>
     <h4 class="font-bold text-1xl mb-2">@ucfirst(__('app.statistics'))</h4>
     <ol class="list-inside list-disc">
         <li>@ucfirst(__('app.numbers_of_exhibitions', ['count' => $exhibitions]))</li>
