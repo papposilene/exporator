d9b24cbb6e15780e88a17d80762d190430f9aac5
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-05 16:20:28 +0100

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="flex space-x-8 text-bluegray-500 dark:text-bluegray-500 text-sm justify-end w-full">
         <div><a href="{{ route('front.about') }}" class="hover:text-red-600 dark:hover:text-red-400">@ucfirst(__('app.about'))</a></div>
-        <div>&copy; 2021 {{ config('app.name', 'Exporator') }}.</div>
+        <div>&copy; 2021-{{ date('Y') }} {{ config('app.name', 'Exporator') }}.</div>
     </div>
 </div>
