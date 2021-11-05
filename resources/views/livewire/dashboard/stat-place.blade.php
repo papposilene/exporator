ae0ebd665a7341fb32e91825abcbb36a9a019dd0
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-05 15:50:59 +0100

    Update stat-place.blade.php

--- resources/views/livewire/dashboard/stat-place.blade.php
+++ resources/views/livewire/dashboard/stat-place.blade.php
@@ -1,5 +1,9 @@
 <div class="flex-grow bg-rose-300 p-5 shadow w-full">
-    <h3 class="font-bold text-2xl mb-5">@ucfirst(__('app.places'))</h3>
+    <h3 class="font-bold text-2xl mb-5">
+        <a href="{{ route('front.place.index') }}">
+            @ucfirst(__('app.places'))
+        </a>
+    </h3>
     <canvas id="chartPlaces" width="400" height="400"></canvas>
     <h4 class="font-bold text-1xl mb-2">@ucfirst(__('app.statistics'))</h4>
     <ol class="list-inside list-disc">
