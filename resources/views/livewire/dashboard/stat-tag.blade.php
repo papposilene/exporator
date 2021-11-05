9740c733b437caa3ec889e0773b10767a7f8f656
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-05 15:50:21 +0100

    Update stat-tag.blade.php

--- resources/views/livewire/dashboard/stat-tag.blade.php
+++ resources/views/livewire/dashboard/stat-tag.blade.php
@@ -1,5 +1,9 @@
 <div class="flex-grow bg-indigo-300 p-5 w-full">
-    <h3 class="font-bold text-2xl mb-5">@ucfirst(__('app.tagcloud'))</h3>
+    <h3 class="font-bold text-2xl mb-5">
+        <a href="{{ route('front.tag.index') }}">
+            @ucfirst(__('app.tagcloud'))
+        </a>
+    </h3>
     @if(count($tags) > 0)
     <div class="grid grid-cols-1 lg:grid-cols-4 sm:gap-2 lg:gap-4">
         @foreach($tags as $tag)
