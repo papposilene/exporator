4dfa44b2825834d0eb6c5eb74e682d42ffa7d431
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-05 16:39:14 +0100

    Update list-tag.blade.php

--- resources/views/livewire/tag/list-tag.blade.php
+++ resources/views/livewire/tag/list-tag.blade.php
@@ -65,10 +65,15 @@
                             <td class="hidden lg:table-cell">
                                 <livewire:interfaces.follow-tag :tag="$tag" :wire:key="$tag->id" />
                             </td>
-                            <td>@ucfirst($tag->type)</td>
+                            <td>
+                                <a href="{{ route('front.tag.type', ['slug' => $tag->type]) }}"
+                                    title="@ucfirst($tag->type)" aria-label="@ucfirst($tag->type)">
+                                    @ucfirst($tag->type)
+                                </a>
+                            </td>
                             <td>
                                 <a href="{{ route('front.tag.show', ['slug' => $tag->slug]) }}"
-                                    title="{{ $tag->name }}" aria-label="{{ $tag->name }}">
+                                    title="@ucfirst($tag->name)" aria-label="@ucfirst($tag->name)">
                                     @ucfirst($tag->name)
                                 </a>
                             </td>
