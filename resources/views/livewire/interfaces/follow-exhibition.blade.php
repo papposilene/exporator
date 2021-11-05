fc31527d8d551713494279c552b3308986158bc2
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-05 11:24:00 +0100

    Update follow-exhibition.blade.php

--- resources/views/livewire/interfaces/follow-exhibition.blade.php
+++ resources/views/livewire/interfaces/follow-exhibition.blade.php
@@ -6,7 +6,7 @@
         <input type="hidden" name="follow" value="{{ $exhibition->hasOne(\App\Models\UserExhibition::class)->first()->uuid }}" />
 
         <x-forms.button class="block mt-1 bg-transparent">
-            <svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 h-6 w-6" fill="yes" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="bg-yellow-500 text-black h-6 w-6" fill="yes" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6M3 17V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
             </svg>
             @ucfirst(__('app.unfollow_the', ['what' => $exhibition->title]))
