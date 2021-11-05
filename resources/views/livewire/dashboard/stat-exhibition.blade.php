b5c836bbc4708cf02d9be1ba7756a421105e56cc
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2021-11-05 16:33:02 +0100

    Update stat-exhibition.blade.php

--- resources/views/livewire/dashboard/stat-exhibition.blade.php
+++ resources/views/livewire/dashboard/stat-exhibition.blade.php
@@ -11,20 +11,4 @@
         <li>@ucfirst(__('app.numbers_of_next_exhibitions', ['count' => $exhibitions_nextmonth]))</li>
         <li>@ucfirst(__('app.numbers_of_finaldays_exhibitions', ['count' => $exhibitions_finaldays]))</li>
     </ol>
-    <!--h4 class="font-bold text-1xl mt-5 mb-2">@ucfirst(__('app.final_days'))</h4>
-    <ol class="list-inside list-disc">
-        @if(count($exhibitions_finaldays) > 0)
-        @foreach($exhibitions_finaldays as $finalday)
-        <li>
-            <a href="{{ route('admin.museum.show', ['slug' => $exhibition->inMuseum->slug]) }}">
-                {{ $exhibition->inMuseum->name }}
-            </a> : {{ $exhibition->title }}.
-        </li>
-        @endforeach
-        @else
-        <li>
-            @ucfirst(__('app.nothing')).
-        </li>
-        @endif
-    </ol-->
 </div>
