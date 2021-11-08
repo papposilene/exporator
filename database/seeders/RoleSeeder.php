42b972cd0e32d5af5e4b4bb4f26d496244b50db7
Philippe-Alexandre Pierre <dev@psln.nl>
2021-11-08 11:15:00 +0100

    backend(permissions): seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -19,10 +19,29 @@ class RoleSeeder extends Seeder
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
         // create permissions
-        Permission::create(['name' => 'edit articles']);
-        Permission::create(['name' => 'delete articles']);
-        Permission::create(['name' => 'publish articles']);
-        Permission::create(['name' => 'unpublish articles']);
+        Permission::create(['name' => 'create places']);
+        Permission::create(['name' => 'update places']);
+        Permission::create(['name' => 'delete places']);
+        Permission::create(['name' => 'publish places']);
+        Permission::create(['name' => 'unpublish places']);
+
+        Permission::create(['name' => 'create exhibitions']);
+        Permission::create(['name' => 'update exhibitions']);
+        Permission::create(['name' => 'delete exhibitions']);
+        Permission::create(['name' => 'publish exhibitions']);
+        Permission::create(['name' => 'unpublish exhibitions']);
+
+        Permission::create(['name' => 'create tags']);
+        Permission::create(['name' => 'update tags']);
+        Permission::create(['name' => 'delete tags']);
+        Permission::create(['name' => 'publish tags']);
+        Permission::create(['name' => 'unpublish tags']);
+
+        Permission::create(['name' => 'create reviews']);
+        Permission::create(['name' => 'update reviews']);
+        Permission::create(['name' => 'delete reviews']);
+        Permission::create(['name' => 'publish reviews']);
+        Permission::create(['name' => 'unpublish reviews']);
 
         // create roles and assign created permissions
 
