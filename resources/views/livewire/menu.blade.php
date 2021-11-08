19fbfbb0ec81d6628ce932755354856d95e01439
Philippe-Alexandre Pierre <dev@psln.nl>
2021-11-08 10:52:52 +0100

    frontend(navigation): direct to current

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -21,14 +21,14 @@
                         :active="request()->routeIs('front.place.*')">
                         @ucfirst(__('app.places'))
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('front.exhibition.index') }}" class="text-bluegray-900 dark:text-bluegray-100"
+                    <x-jet-nav-link href="{{ route('front.exhibition.index', ['filter' => 'current']) }}" class="text-bluegray-900 dark:text-bluegray-100"
                         :active="request()->routeIs(['front.exhibition.index', 'front.exhibition.show'])">
                         @ucfirst(__('app.exhibitions'))
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('front.exhibition.calendar') }}"  class="text-bluegray-900 dark:text-bluegray-100"
+                    <!-- x-jet-nav-link href="{{ route('front.exhibition.calendar') }}"  class="text-bluegray-900 dark:text-bluegray-100"
                         :active="request()->routeIs('front.exhibition.calendar')">
                         @ucfirst(__('app.calendar'))
-                    </x-jet-nav-link>
+                    </x-jet-nav-link -->
                     <x-jet-nav-link href="{{ route('front.exhibition.map') }}"  class="text-bluegray-900 dark:text-bluegray-100"
                         :active="request()->routeIs('front.exhibition.map')">
                         @ucfirst(__('app.map'))
@@ -52,7 +52,7 @@
                                         {{ Auth::user()->currentTeam->name }}
 
                                         <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" 
+                                            <path fill-rule="evenodd"
                                                 d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
                                         </svg>
                                     </button>
@@ -153,7 +153,7 @@
                         <a href="{{ route('login') }}" title="@ucfirst(__('auth.login'))"
                             class="hover:text-black">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
                             </svg>
                         </a>
@@ -183,12 +183,12 @@
             <x-jet-responsive-nav-link href="{{ route('front.place.index') }}" :active="request()->routeIs('front.place.*')">
                 @ucfirst(__('app.places'))
             </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('front.exhibition.index') }}" :active="request()->routeIs('front.exhibition.*')">
+            <x-jet-responsive-nav-link href="{{ route('front.exhibition.index', ['filter' => 'current']) }}" :active="request()->routeIs('front.exhibition.*')">
                 @ucfirst(__('app.exhibitions'))
             </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('front.exhibition.calendar') }}" :active="request()->routeIs('front.exhibition.calendar')">
+            <!-- x-jet-responsive-nav-link href="{{ route('front.exhibition.calendar') }}" :active="request()->routeIs('front.exhibition.calendar')">
                 @ucfirst(__('app.calendar'))
-            </x-jet-responsive-nav-link>
+            </x-jet-responsive-nav-link -->
             <x-jet-responsive-nav-link href="{{ route('front.exhibition.map') }}" :active="request()->routeIs('front.exhibition.map')">
                 @ucfirst(__('app.map'))
             </x-jet-responsive-nav-link>
