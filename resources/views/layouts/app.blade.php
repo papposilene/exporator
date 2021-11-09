c054c660200782df3cfcc4ee3b857b7b6a536830
Philippe-Alexandre Pierre <dev@psln.nl>
2021-11-09 21:12:01 +0100

    frontend(footer): bg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
                 {{ $slot }}
             </main>
 
-            <footer class="flex bg-bluegray-200 dark:bg-gray-800 shadow">
+            <footer class="flex bg-bluegray-100 dark:bg-gray-700 shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 w-full">
                     <x-footer />
                 </div>
