2c3bf1e34bc797c8b5ee9d5c8f7db769ba89ba5d
Philippe-Alexandre Pierre <dev@psln.nl>
2021-11-09 11:59:14 +0100

    frontend(modals): add darkmode support

--- resources/views/livewire/modals/create-place.blade.php
+++ resources/views/livewire/modals/create-place.blade.php
@@ -18,6 +18,9 @@
                         d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
             </button>
+            <h5 class="text-xl text-bold">
+                @ucfirst(__('app.create_one', ['what' => __('app.place')]))
+            </h5>
             <!-- Modal content -->
             <form method="POST" action="{{ route('admin.place.store') }}" enctype="multipart/form-data"
                 class="flex flex-col w-full">
