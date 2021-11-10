b92fa6e2ae0a6b2140d3ea1426157b15a17e2ca3
Philippe-Alexandre Pierre <dev@psln.nl>
2021-11-10 07:49:57 +0100

    frontend(user and tag): rounded buttons

--- resources/views/livewire/tag/list-tag.blade.php
+++ resources/views/livewire/tag/list-tag.blade.php
@@ -30,13 +30,13 @@
                     <a href="?filter=" class="flex flex-auto text-base hover:scale-110 focus:outline-none
                         justify-center px-4 py-2 rounded font-bold cursor-pointer hover:text-black
                         hover:bg-bluegray-300 bg-bluegray-200 hover:border-bluegray-400
-                        border duration-200 ease-in-out transition">
+                        border duration-200 ease-in-out transition lg:rounded-r-none">
                         <div class="flex leading-5">@ucfirst(__('app.all'))</div>
                     </a>
                     <a href="?filter=followed" class="flex flex-auto text-base hover:scale-110 focus:outline-none
                         justify-center px-4 py-2 rounded font-bold cursor-pointer hover:text-black
                         hover:bg-bluegray-300 bg-bluegray-200 hover:border-bluegray-400
-                        border duration-200 ease-in-out transition">
+                        border duration-200 ease-in-out transition lg:rounded-l-none">
                         <div class="flex leading-5">
                             <svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 h-6 w-6" fill="yes" viewBox="0 0 24 24" stroke="currentColor" title="@ucfirst(__('app.followed'))">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
