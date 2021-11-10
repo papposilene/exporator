b4e5f570c7777f7c47b41b2b3ef9c1ca78622c8e
Philippe-Alexandre Pierre <dev@psln.nl>
2021-11-10 18:09:53 +0100

    backend(activity): show

--- resources/views/livewire/user/show-activity.blade.php
+++ resources/views/livewire/user/show-activity.blade.php
@@ -1,4 +1,4 @@
-@section('title', @ucfirst($type->type))
+@section('title', @ucfirst(__('activity.activity')))
 
 <div>
     <x-slot name="header">
@@ -13,7 +13,28 @@
     </x-slot>
 
     <div class="flex flex-wrap w-full max-w-7xl mx-auto">
-        <div class="mx-auto py-5 px-6 w-full">
+        <div class="mx-auto md:w-1/4 py-5 px-6 w-full">
+            <ul class="bg-teal-100 md:m-5 mt-5 md:mt-0 p-5 shadow w-full">
+                <li class="font-semibold text-red-500 text-center text-2xl mb-3">
+                    {{ $users->find($activity->causer_id)->getRoleNames()->first() }}
+                </li>
+                <li class="text-xl font-bold mb-3">
+                    <a href="{{ route('front.user.show', ['uuid' => $users->find($activity->causer_id)->uuid]) }}">
+                        {{ $users->find($activity->causer_id)->name }}
+                    </a>
+                </li>
+                <li>@datetime($users->find($activity->causer_id)->created_at)</li>
+                <li>
+                    <ol class="list-inside list-disc w-full">
+                        <li>{{ $users->find($activity->causer_id)->followedPlaces()->count() }}</li>
+                        <li>{{ $users->find($activity->causer_id)->followedExhibitions()->count() }}</li>
+                        <li>{{ $users->find($activity->causer_id)->followedTags()->count() }}</li>
+                    </ol>
+                </li>
+            </ul>
+        </div>
+
+        <div class="mx-auto md:w-3/4 py-5 px-6">
             @if ($errors->any())
             <div class="bg-red-500 border border-red-700 mb-3 p-3 rounded shadow text-white font-bold">
                 <ul>
@@ -23,16 +44,7 @@
                 </ul>
             </div>
             @endif
-
-            <div class="flex sm:flex-row lg:flex-col flex-grow flex-wrap bg-bluegray-500 text-white lg:mr-3 p-5 shadow sm:w-full">
-                <p class="mb-3">
-                    {{ $activity->id }}
-                </p>
-                <p class="mb-3">
-                    {{ $activity->subject_id }}
-                </p>
-
-                {{ dd($users->find($activity->subject_id)) }}
+            <div class="bg-teal-500 py-5 shadow">
 
             </div>
         </div>
