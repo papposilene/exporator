9c7d2b31d1d8e2daa5e6de2beef7d208e3dc93b4
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-11-16 18:07:41 +0100

    export(exhibition): controller

--- app/Exports/ExhitionsExport.php
+++ app/Exports/ExhitionsExport.php
@@ -4,8 +4,10 @@ namespace App\Exports;
 
 use App\Models\Exhibition;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class ExhibitionsExport implements FromCollection
+class ExhibitionsExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +15,35 @@ class ExhibitionsExport implements FromCollection
     public function collection()
     {
         return Exhibition::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'uuid',
+            'place',
+            'title',
+            'began_at',
+            'ended_at',
+            'price',
+            'description',
+            'link',
+            'tags',
+        ];
+    }
+
+    public function map($exhibition): array
+    {
+        return [
+            $exhibition->uuid,
+            $exhibition->inPlace->name,
+            $exhibition->title,
+            $exhibition->began_at->format('d/m/Y'),
+            $exhibition->ended_at->format('d/m/Y'),
+            $exhibition->price,
+            $exhibition->description,
+            $exhibition->link,
+            $exhibition->isTagged(),
+        ];
     }
 }
