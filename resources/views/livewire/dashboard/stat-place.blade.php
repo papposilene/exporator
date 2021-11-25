4532315083ea69e356729848b89b103f0549cd9a
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-11-25 19:26:13 +0100

    lang(fr): dashboard

--- resources/views/livewire/dashboard/stat-place.blade.php
+++ resources/views/livewire/dashboard/stat-place.blade.php
@@ -14,49 +14,49 @@
         </div>
         @endauth
         <div class="bg-rose-500 text-white p-2 rounded">
-            <a href="{{ route('front.place.index', ['filter' => '']) }}" class="flex flex-grow justify-between">
+            <a href="{{ route('front.place.index', ['type' => '']) }}" class="flex flex-grow justify-between">
                 <span class="px-3">@ucfirst(__('app.numbers_of_places'))</span>
                 <span class="px-3">{{ $places }}</span>
             </a>
         </div>
         <div class="bg-rose-500 text-white p-2 rounded">
-            <a href="{{ route('front.place.index', ['filter' => 'museum']) }}" class="flex flex-grow justify-between">
+            <a href="{{ route('front.place.index', ['type' => 'museum']) }}" class="flex flex-grow justify-between">
                 <span class="px-3">@ucfirst(__('app.numbers_of_museum_type'))</span>
                 <span class="px-3">{{ $museum_type }}</span>
             </a>
         </div>
         <div class="bg-rose-500 text-white p-2 rounded">
-            <a href="{{ route('front.place.index', ['filter' => 'gallery']) }}" class="flex flex-grow justify-between">
+            <a href="{{ route('front.place.index', ['type' => 'gallery']) }}" class="flex flex-grow justify-between">
                 <span class="px-3">@ucfirst(__('app.numbers_of_gallery_type'))</span>
                 <span class="px-3">{{ $gallery_type }}</span>
             </a>
         </div>
         <div class="bg-rose-500 text-white p-2 rounded">
-            <a href="{{ route('front.place.index', ['filter' => 'art-center']) }}" class="flex flex-grow justify-between">
+            <a href="{{ route('front.place.index', ['type' => 'art-center']) }}" class="flex flex-grow justify-between">
                 <span class="px-3">@ucfirst(__('app.numbers_of_artcenter_type'))</span>
                 <span class="px-3">{{ $artcenter_type }}</span>
             </a>
         </div>
         <div class="bg-rose-500 text-white p-2 rounded">
-            <a href="{{ route('front.place.index', ['filter' => 'art-fair']) }}" class="flex flex-grow justify-between">
+            <a href="{{ route('front.place.index', ['type' => 'art-fair']) }}" class="flex flex-grow justify-between">
                 <span class="px-3">@ucfirst(__('app.numbers_of_artfair_type'))</span>
                 <span class="px-3">{{ $artfair_type }}</span>
             </a>
         </div>
         <div class="bg-rose-500 text-white p-2 rounded">
-            <a href="{{ route('front.place.index', ['filter' => 'library']) }}" class="flex flex-grow justify-between">
+            <a href="{{ route('front.place.index', ['type' => 'library']) }}" class="flex flex-grow justify-between">
                 <span class="px-3">@ucfirst(__('app.numbers_of_library_type'))</span>
                 <span class="px-3">{{ $library_type }}</span>
             </a>
         </div>
         <div class="bg-rose-500 text-white p-2 rounded">
-            <a href="{{ route('front.place.index', ['filter' => 'foundation']) }}" class="flex flex-grow justify-between">
+            <a href="{{ route('front.place.index', ['type' => 'foundation']) }}" class="flex flex-grow justify-between">
                 <span class="px-3">@ucfirst(__('app.numbers_of_foundation_type'))</span>
                 <span class="px-3">{{ $foundation_type }}</span>
             </a>
         </div>
         <div class="bg-rose-500 text-white p-2 rounded">
-            <a href="{{ route('front.place.index', ['filter' => 'other']) }}" class="flex flex-grow justify-between">
+            <a href="{{ route('front.place.index', ['type' => 'other']) }}" class="flex flex-grow justify-between">
                 <span class="px-3">@ucfirst(__('app.numbers_of_other_type'))</span>
                 <span class="px-3">{{ $other_type }}</span>
             </a>
