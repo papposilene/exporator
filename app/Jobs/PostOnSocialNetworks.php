b95694aa5106111ef165427bb0842b1955617d59
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2022-05-04 15:29:35 +0200

    Update PostOnSocialNetworks.php

--- app/Jobs/PostOnSocialNetworks.php
+++ app/Jobs/PostOnSocialNetworks.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Exhibition;
 use Abraham\TwitterOAuth\TwitterOAuth;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
@@ -19,9 +20,11 @@ class PostOnSocialNetworks implements ShouldQueue
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Exhibition $exhibition)
     {
-        //
+        $this->onQueue('socialnetworking');
+        $this->exhibition = $exhibition;
+        
     }
 
     /**
@@ -31,6 +34,35 @@ class PostOnSocialNetworks implements ShouldQueue
      */
     public function handle()
     {
-        //
+        if ($this->exhibition) {
+            // Twitter API: set up the connection before tweeting about the new place
+            try {
+                $twitter = new TwitterOAuth(
+                    env('TWITTER_EXPORATOR_CONSUMERKEY', false),
+                    env('TWITTER_EXPORATOR_CONSUMERSECRET', false),
+                    env('TWITTER_EXPORATOR_TOKEN', false),
+                    env('TWITTER_EXPORATOR_TOKENSECRET', false)
+                );
+            }
+            catch (\Throwable $e) {
+                report('Twitter: error during the connection for a new place.');
+            }
+            
+            try {
+                $tweet = ucfirst(__('app.send_exhibition_tweet', [
+                    'what' => $this->exhibition->title,
+                    'twitter' => $this->exhibition->inPlace->twitter,
+                    'url' => route('front.place.show', ['slug' => $this->exhibition->inPlace->slug]),
+                    'site' => $this->exhibition->link,
+                ]));
+
+                $twitter->post('statuses/update', [
+                    'status' => $tweet,
+                ]);
+            }
+            catch (\Throwable $e) {
+                report('Twitter: error during tweeting a new place.');
+            }
+        }
     }
 }
