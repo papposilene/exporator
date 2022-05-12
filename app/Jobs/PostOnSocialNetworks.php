687e125722098c1e0f2ce88d07a224facaac46c5
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-05-12 10:09:15 +0200

    Jobs: add Mastodon to social networks

--- app/Jobs/PostOnSocialNetworks.php
+++ app/Jobs/PostOnSocialNetworks.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Revolution\Mastodon\Facades\Mastodon;
 
 class PostOnSocialNetworks implements ShouldQueue
 {
@@ -31,6 +32,21 @@ class PostOnSocialNetworks implements ShouldQueue
         $this->exhibition = $exhibition;
     }
 
+    private function setTwitter()
+    {
+        try {
+            return new TwitterOAuth(
+                env('TWITTER_EXPORATOR_CONSUMERKEY', false),
+                env('TWITTER_EXPORATOR_CONSUMERSECRET', false),
+                env('TWITTER_EXPORATOR_TOKEN', false),
+                env('TWITTER_EXPORATOR_TOKENSECRET', false)
+            );
+        }
+        catch (\Throwable $e) {
+            report('Twitter: error during the connection for a new place.');
+        }
+    }
+
     /**
      * Execute the job.
      *
@@ -39,33 +55,28 @@ class PostOnSocialNetworks implements ShouldQueue
     public function handle()
     {
         if ($this->exhibition) {
-            // Twitter API: set up the connection before tweeting about the new place
+            $status = ucfirst(__('app.send_exhibition_tweet', [
+                'what' => $this->exhibition->title,
+                'twitter' => $this->exhibition->inPlace->twitter,
+                'url' => route('front.place.show', ['slug' => $this->exhibition->inPlace->slug]),
+                'site' => $this->exhibition->link,
+            ]));
+
             try {
-                $twitter = new TwitterOAuth(
-                    env('TWITTER_EXPORATOR_CONSUMERKEY', false),
-                    env('TWITTER_EXPORATOR_CONSUMERSECRET', false),
-                    env('TWITTER_EXPORATOR_TOKEN', false),
-                    env('TWITTER_EXPORATOR_TOKENSECRET', false)
-                );
+                Mastodon::domain('https://mastodon.art')->token(env('MASTODON_EXPORATOR_TOKEN', false));
+                Mastodon::createStatus($status, ['language' => app()->getLocale()]);
             }
             catch (\Throwable $e) {
-                report('Twitter: error during the connection for a new place.');
+                report('Mastodon: error during posting a new exhibition.');
             }
 
             try {
-                $tweet = ucfirst(__('app.send_exhibition_tweet', [
-                    'what' => $this->exhibition->title,
-                    'twitter' => $this->exhibition->inPlace->twitter,
-                    'url' => route('front.place.show', ['slug' => $this->exhibition->inPlace->slug]),
-                    'site' => $this->exhibition->link,
-                ]));
-
-                $twitter->post('statuses/update', [
-                    'status' => $tweet,
+                $this->setTwitter()->post('statuses/update', [
+                    'status' => $status,
                 ]);
             }
             catch (\Throwable $e) {
-                report('Twitter: error during tweeting a new place.');
+                report('Twitter: error during tweeting a new exhibition.');
             }
         }
     }
