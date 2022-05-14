1f8d9a792caf65abb29ea016cb7d06e1d8cd6ad3
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-05-14 19:43:20 +0200

    Livewire: update controllers and views

--- app/Http/Controllers/Backend/PlaceController.php
+++ app/Http/Controllers/Backend/PlaceController.php
@@ -12,11 +12,13 @@ use App\Imports\PlacesImport;
 use App\Models\Country;
 use App\Models\Place;
 use App\Models\User;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Facades\Excel;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class PlaceController extends Controller
 {
@@ -27,30 +29,10 @@ class PlaceController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\StorePlaceRequest  $request
-     * @return \Illuminate\Http\Response
+     * @param StorePlaceRequest $request
+     * @return RedirectResponse
      */
     public function store(StorePlaceRequest $request)
     {
@@ -62,8 +44,7 @@ class PlaceController extends Controller
         $name = $request->input('name');
         $slug = Str::slug($request->input('city') . ' ' . $name, '-');
 
-        if ($request->hasFile('image') && $request->file('image')->isValid())
-        {
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $path = $request->image->storeAs(
                 'places', $slug
             );
@@ -73,7 +54,7 @@ class PlaceController extends Controller
         $place->slug = $slug;
         $place->name = $name;
         $place->type = $request->input('type');
-        $place->status = (bool) $request->input('status');
+        $place->status = (bool)$request->input('status');
         $place->address = $request->input('address');
         $place->city = $request->input('city');
         $place->country_cca3 = $country->cca3;
@@ -84,33 +65,35 @@ class PlaceController extends Controller
         $place->image = ($request->hasFile('image') ? $path : null);
         $place->save();
 
-        // Twitter API: set up the connection before tweeting about the new place
-        try {
-            $twitter = new TwitterOAuth(
-                env('TWITTER_EXPORATOR_CONSUMERKEY', null),
-                env('TWITTER_EXPORATOR_CONSUMERSECRET', null),
-                env('TWITTER_EXPORATOR_TOKEN', null),
-                env('TWITTER_EXPORATOR_TOKENSECRET', null)
-            );
-            $twitter->setApiVersion('1.1');
+        if ($place->twitter) {
 
+
+            // Twitter API: set up the connection before tweeting about the new place
             try {
-                $tweet = ucfirst(__('app.send_place_tweet', [
-                    'what' => $name,
-                    'twitter' => $request->input('twitter'),
-                    'url' => route('front.place.show', ['slug' => $slug]),
-                ]));
+                $twitter = new TwitterOAuth(
+                    env('TWITTER_EXPORATOR_CONSUMERKEY', null),
+                    env('TWITTER_EXPORATOR_CONSUMERSECRET', null),
+                    env('TWITTER_EXPORATOR_TOKEN', null),
+                    env('TWITTER_EXPORATOR_TOKENSECRET', null)
+                );
+                $twitter->setApiVersion('1.1');
 
-                $twitter->post('statuses/update', [
-                    'status' => $tweet,
-                ]);
+                try {
+                    $tweet = ucfirst(__('app.send_place_tweet', [
+                        'what' => $name,
+                        'twitter' => $request->input('twitter'),
+                        'url' => route('front.place.show', ['slug' => $slug]),
+                    ]));
+
+                    $twitter->post('statuses/update', [
+                        'status' => $tweet,
+                    ]);
+                } catch (\Throwable $e) {
+                    report('Twitter: error during tweeting a new place.');
+                }
+            } catch (\Throwable $e) {
+                report('Twitter: error during the connection for a new place.');
             }
-            catch (\Throwable $e) {
-                report('Twitter: error during tweeting a new place.');
-            }
-        }
-        catch (\Throwable $e) {
-            report('Twitter: error during the connection for a new place.');
         }
 
         return redirect()->route('front.place.show', ['slug' => $slug])->with('success', 'All good!');
@@ -119,8 +102,9 @@ class PlaceController extends Controller
     /**
      * Import a file for creating a new resource.
      *
-     * @param  \Illuminate\Http\ImportPlaceRequest  $request
-     * @return \Illuminate\Http\Response
+     * @param ImportPlaceRequest $request
+     * @return RedirectResponse
+     * @throws AuthorizationException
      */
     public function import(ImportPlaceRequest $request)
     {
@@ -128,12 +112,10 @@ class PlaceController extends Controller
 
         $validated = $request->validated();
 
-        try
-        {
+        try {
             $import = new PlacesImport();
             $import->import($request->file('datafile'));
-        } catch (\Maatwebsite\Excel\Validators\ValidationException $e)
-        {
+        } catch (\Maatwebsite\Excel\Validators\ValidationException $e) {
             Storage::delete($request->file('datafile'));
             $failures = $e->failures();
 
@@ -148,33 +130,12 @@ class PlaceController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Place  $place
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Place $place)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Place  $place
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Place $place)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\UpdatePlaceRequest  $request
-     * @param  \App\Models\Place  $place
-     * @return \Illuminate\Http\Response
+     * @param UpdatePlaceRequest $request
+     * @param Place $place
+     * @return RedirectResponse
+     * @throws AuthorizationException
      */
     public function update(UpdatePlaceRequest $request, Place $place)
     {
@@ -185,8 +146,7 @@ class PlaceController extends Controller
         $country = Country::where('cca3', $request->input('cca3'))->firstOrFail();
         $slug = Str::slug($request->input('city') . ' ' . $request->input('name'), '-');
 
-        if ($request->hasFile('image') && $request->file('image')->isValid())
-        {
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $path = $request->image->storeAs(
                 'places', $slug
             );
@@ -196,7 +156,7 @@ class PlaceController extends Controller
         $place->slug = $slug;
         $place->name = $request->input('name');
         $place->type = $request->input('type');
-        $place->status = (bool) $request->input('status');
+        $place->status = (bool)$request->input('status');
         $place->address = $request->input('address');
         $place->city = $request->input('city');
         $place->country_cca3 = $country->cca3;
@@ -213,22 +173,11 @@ class PlaceController extends Controller
     /**
      * Export all the resources from storage.
      *
-     * @return \Illuminate\Http\Response
+     * @return BinaryFileResponse
      */
     public function export()
     {
         $date = date('Y-m-d');
         return Excel::download(new PlacesExport, $date . '_places.xlsx');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Place  $place
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Place $place)
-    {
-        //
     }
 }
